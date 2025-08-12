68bd62011b636aceceb1e6d91ddcfe15b20d5a88
athosg <athrael.soju@gmail.com>
2025-08-12 17:33:32 +0100

    Update install_cuda.sh

--- colqwen_fastapi/install_cuda.sh
+++ colqwen_fastapi/install_cuda.sh
@@ -1,2 +1,2 @@
+# Install CUDA optionally if your Nvidia GPU isn't yet supported by the latest version of PyTorch
 uv pip install torch torchvision --index-url https://download.pytorch.org/whl/cu128 --upgrade
-
